7653b54cd3fc0a43fc50f164de1eee77212abdca
Vinicius de Assis Azevedo <viniciusdeassisazevedo@hotmail.com>
2022-01-09 13:52:27 -0300

    Estudando um pouco sobre evenetos

---
+++ Estudo 01/React01/src/components/Form.js
@@ -0,0 +1,23 @@
+function Form(){
+    function cadastrarUsuario(event){
+        event.preventDefault()
+        console.log('Cadastrou usuario...')
+    }
+    return(
+        <div>
+            <h1>
+                Meu cadastro
+            </h1>
+            <form onSubmit={cadastrarUsuario} >
+                <div>
+                    <input type='text' placeholder='Digite seu nome'/>
+                </div>
+                <div>
+                    <input type = 'submit' value='Cadastrar'/>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form
